b951ae5a6e87586cc2615e7059ddfaa9affee1a1
pwehbe <pwehbe@atheerair.com>
2018-10-17 18:42:30 -0700

    Initial Commit

---
+++ CursorTests/CursorTests.swift
@@ -0,0 +1,34 @@
+//
+//  CursorTests.swift
+//  CursorTests
+//
+//  Created by Pierre WEHBE on 10/17/18.
+//  Copyright © 2018 Atheer. All rights reserved.
+//
+
+import XCTest
+@testable import Cursor
+
+class CursorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
